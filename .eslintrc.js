50c2f8f391a9974d24a6eda7ff2b480e1d8573c9
Tdrj2716 <ri76joy@gmail.com>
2021-08-19 23:03:59 +0900

    setup for development by react and typescript

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended", //no-unused-varsのエラーを非表示にするために追加
+    "prettier", // configを参照するために追加
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  plugins: ["react", "@typescript-eslint"],
+  rules: {},
+};
